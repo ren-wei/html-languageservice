dbef4e1068f78be9693e7b20dba4f94c29336493
ren-wei <demonre@foxmail.com>
2025-04-25 14:44:37 +0800

    chore: add parse-speed example

---
+++ examples/parse-speed/main.rs
@@ -0,0 +1,16 @@
+use std::{fs, time};
+
+use html_languageservice::{HTMLDataManager, HTMLLanguageService};
+use lsp_textdocument::FullTextDocument;
+
+fn main() {
+    let content = fs::read_to_string("examples/parse-speed/index.html").unwrap();
+
+    let document = FullTextDocument::new("html".to_string(), 0, content);
+
+    let start_time = time::SystemTime::now();
+    HTMLLanguageService::parse_html_document(&document, &HTMLDataManager::default());
+    let end_time = time::SystemTime::now();
+    let duration = end_time.duration_since(start_time).unwrap();
+    println!("{:?}", duration);
+}
